5c79eeeafb1c3b9c3654197c329e79a944290b5b
Ulyana <abram.jully@gmail.com>
2021-05-15 21:09:42 +0300

    Added input

---
+++ input.py
@@ -0,0 +1,19 @@
+import numpy as np
+from LU_decomposition import get_LU_decomposition, get_reverse_matrix
+from scipy.sparse import csr_matrix
+
+n = int(input("Enter dim of matrix: "))
+print("Enter matrix:")
+a = np.zeros((n, n))
+for i in range(n):
+    b = list(map(float, input().split()))
+    for j in range(n):
+        a[i][j] = b[j]
+
+print("LU decomposition:")
+L, U = get_LU_decomposition(csr_matrix(a))
+print(L.toarray(), '\n')
+print(U.toarray(), '\n')
+
+print("Reverse matrix:")
+print(get_reverse_matrix(csr_matrix(a)).toarray(), '\n')
